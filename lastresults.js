1d8790e7a7cf7b1b85c05b49e7d09133c2ba4d90
Adina <98980934+Adina8Gr@users.noreply.github.com>
2024-01-15 21:16:26 +0200

    +celelalte pagini

---
+++ lastresults.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Obține elementul cu id-ul "navbar"
+  var navbar = document.getElementById('navbar');
+
+  // Adaugă un ascultător de eveniment de clic pe bara de navigare
+  navbar.addEventListener('click', function (event) {
+    // Verifică dacă elementul pe care s-a făcut clic este un element "div" cu clasa "text-wrapper-9"
+    if (event.target.tagName === 'DIV' && event.target.classList.contains('text-wrapper-9')) {
+      // Obține textul din interiorul elementului pe care s-a făcut clic
+      var buttonText = event.target.innerText;
+
+      // Redirecționează către pagina corespunzătoare în funcție de textul butonului
+      switch (buttonText) {
+        case 'JUCATORI':
+          window.location.href = 'players1.html'; // Schimbă cu numele real al paginii CLUB
+          break;
+        case 'CLUB':
+          window.location.href = 'club.html';
+          break;
+          case 'CALENDAR':
+            window.location.href = 'nextfixtures.html';
+            break;
+            case 'NOUTATI':
+              window.location.href = 'news.html';
+              break;
+              case 'Meciuri viitoare':
+                window.location.href = 'nextfixtures.html';
+                break;
+                case 'Ultimele rezultate':
+                  window.location.href = 'lastresults.html';
+                  break;      
+        // Adaugă cazuri pentru celelalte pagini
+      }
+    }
+  });
+});
+function changeColor(id)
+{
+document.getElementById(id).style.color = "#ff0000"; // forecolor
+document.getElementById(id).style.backgroundColor = "#ff0000"; // backcolor
+};
--- nextfixtures.js
+++ nextfixtures.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Adaugă un ascultător de eveniment de clic pe bara de navigare
     navbar.addEventListener('click', function (event) {
       // Verifică dacă elementul pe care s-a făcut clic este un element "div" cu clasa "text-wrapper-9"
-      if (event.target.tagName === 'DIV' && event.target.classList.contains('text-wrapper-6')) {
+      if (event.target.tagName === 'DIV' && event.target.classList.contains('text-wrapper-9')) {
         // Obține textul din interiorul elementului pe care s-a făcut clic
         var buttonText = event.target.innerText;
 
@@ -23,14 +23,19 @@ document.addEventListener('DOMContentLoaded', function () {
               case 'NOUTATI':
                 window.location.href = 'news.html';
                 break;
-          case 'Portari':
-            window.location.href = 'players1.html'; // Schimbă cu numele real al paginii CLUB
-            break;
-          case 'Interi':
-            window.location.href = 'players2.html'; // Schimbă cu numele real al paginii CALENDAR
-            break;
+                case 'Meciuri viitoare':
+                  window.location.href = 'nextfixtures.html';
+                  break;
+                  case 'Ultimele rezultate':
+                    window.location.href = 'lastresults.html';
+                    break;      
           // Adaugă cazuri pentru celelalte pagini
         }
       }
     });
   });
+  function changeColor(id)
+{
+  document.getElementById(id).style.color = "#ff0000"; // forecolor
+  document.getElementById(id).style.backgroundColor = "#ff0000"; // backcolor
+};
